3db022aa4cfd0b9842ce494379c0eb71492b14e7
Andrea Dal Molin <andrea.dalmolin@student.hepl.be>
2023-04-30 21:30:07 +0200

    Completed secured exchange logic

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -5,6 +5,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,11 +19,14 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 import java.util.Base64;
 
+import static utils.CryptoUtil.decryptStringWithAES;
+import static utils.CryptoUtil.encryptStringWithAES;
+
 public class Bank {
     private static int port = 5000;
     private static final String CERT_PASSWD = "dalgov";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SignatureException {
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
@@ -40,26 +44,61 @@ public class Bank {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected from " + clientSocket.getInetAddress());
 
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
                 InputStream in = clientSocket.getInputStream();
                 ObjectInputStream objIn = new ObjectInputStream(in);
-                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                Object inputStream = objIn.readObject();
 
-                Object obj = objIn.readObject();
-
-                if (obj instanceof String str) {
-                    // Decrypt
+                if (inputStream instanceof String) {
+                    // Decrypt and extract Signature, Session Key and Client's EID Public Key
                     Cipher cipher2 = Cipher.getInstance("ECIES", "BC");
                     cipher2.init(Cipher.DECRYPT_MODE, privateKey1);
-                    byte[] decoded = Base64.getDecoder().decode(((String) obj).getBytes(StandardCharsets.UTF_8));
+                    byte[] decoded = Base64.getDecoder().decode(((String) inputStream).getBytes(StandardCharsets.UTF_8));
                     byte[] messageBytes = cipher2.doFinal(decoded);
                     String message = new String(messageBytes);
                     byte[] signature = Base64.getDecoder().decode(message.split("#")[0]);
                     byte[] sessionKey = Base64.getDecoder().decode(message.split("#")[1]);
-                    byte[] clientPubKey = Base64.getDecoder().decode(message.split("#")[2]);
+                    byte[] clientPubKeyBytes = Base64.getDecoder().decode(message.split("#")[2]);
+                    X509EncodedKeySpec keySpec = new X509EncodedKeySpec(clientPubKeyBytes);
+                    KeyFactory keyFactory = KeyFactory.getInstance("EC");
+                    ECPublicKey ecPublicKey = (ECPublicKey) keyFactory.generatePublic(keySpec);
 
+                    // Verify if the signature is valid
+                    if (!checkSignatureValid(ecPublicKey, signature, sessionKey)) {
+                        //TODO
+                        //Stop the process, signature invalid
+                    }
 
+                    out.writeObject(encryptStringWithAES("CLIENT_NUMBER_REQUEST", sessionKey));
+                    out.flush();
+
+                    // Retrieve client number
+                    inputStream = objIn.readObject();
+
+                    //Here you do all the stuff involved with fecthing the user in the DB, checking if he exists, if the number is valid etc...
+                    // ...
+
+                    // Generate random challenge and send it to client
+                    byte[] challenge = RandomChallengeGenerator.generateRandomChallenge(32);
+                    String encodedChallenge = Base64.getEncoder().encodeToString(challenge);
+                    out.writeObject(encryptStringWithAES(encodedChallenge, sessionKey));
+                    out.flush();
+
+                    inputStream = decryptStringWithAES((byte[]) objIn.readObject(), sessionKey);
+
+                    byte[] signedChallenge = Base64.getDecoder().decode((String) ((String) inputStream).split("#")[0]);
+                    String userPin = (String) ((String) inputStream).split("#")[1];
+
+                    //TODO verify user's PIN
+
+                    if (checkSignatureValid(ecPublicKey, signedChallenge, challenge)) {
+                        out.writeObject(encryptStringWithAES("OK", sessionKey));
+                        out.flush();
+                    } else {
+                        out.writeObject(encryptStringWithAES("NOK", sessionKey));
+                        out.flush();
+                    }
                 }
-
 
                 objIn.close();
                 in.close();
@@ -85,7 +124,16 @@ public class Bank {
             throw new RuntimeException(e);
         } catch (InvalidKeyException e) {
             throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
         }
 
+    }
+
+    private static boolean checkSignatureValid(ECPublicKey ecPublicKey, byte[] signature, byte[] originalMessage) throws  NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        Signature sVerif = Signature.getInstance("SHA384withECDSA");
+        sVerif.initVerify(ecPublicKey);
+        sVerif.update(originalMessage);
+        return sVerif.verify(signature);
     }
 }
--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,7 +5,6 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -16,10 +15,15 @@ import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Base64;
 
+import static utils.CryptoUtil.decryptStringWithAES;
+import static utils.CryptoUtil.encryptStringWithAES;
+
 public class Client {
     private static final int SERVER_PORT = 5000;
     private static final String SERVER_IP = "127.0.0.1";
     private static final String CERT_PASSWD = "dalgov";
+    private static final String USER_PIN = "1111";
+    private static final String CLIENT_NUMBER = "1111";
 
     public static void main(String[] args) {
         EIDKeystoreManager keyStore = new EIDKeystoreManager();
@@ -34,45 +38,72 @@ public class Client {
     private static void getBankAuthorization(PublicKey publicKey, PrivateKey privateKey) {
         try {
             //TODO Fetch bank's certificate from Certificate Server
+            Security.addProvider(new BouncyCastleProvider());
             KeyStore keyStore = KeyStore.getInstance("PKCS12");
             FileInputStream fis = new FileInputStream("src/main/certs/keystore.jks");
             keyStore.load(fis, CERT_PASSWD.toCharArray());
             Certificate bankCert = keyStore.getCertificate("beid");
             PublicKey bankPubKey = bankCert.getPublicKey();
-            PrivateKey privateKey1 = (PrivateKey) keyStore.getKey("beid", CERT_PASSWD.toCharArray());
-
-            Security.addProvider(new BouncyCastleProvider());
-
-            byte[] sessionKey = RandomChallengeGenerator.generateRandomChallenge(32);
+//            PrivateKey bankPrivKey = (PrivateKey) keyStore.getKey("beid", CERT_PASSWD.toCharArray());
 
             // Connect to the server
             try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
-                var signature = signSessionKey(sessionKey, privateKey);
+                InputStream in = socket.getInputStream();
+                ObjectInputStream inputStream = new ObjectInputStream(in);
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                
+                // 1. Generate a session key and sign it 
+                byte[] sessionKey = RandomChallengeGenerator.generateRandomChallenge(32);
+                var signature = signMessage(sessionKey, privateKey);
 
+                // 2. Send Session key + Signature + EID Public Key to the bank
                 String sessionKeyAndSignature =
                         Base64.getEncoder().encodeToString(signature)
                         + "#"
                         + Base64.getEncoder().encodeToString(sessionKey)
                         + "#"
-                        + Base64.getEncoder().encodeToString(publicKey.getEncoded())
-                        + "clientNumber";
+                        + Base64.getEncoder().encodeToString(publicKey.getEncoded());
 
                 Cipher cipher = Cipher.getInstance("ECIES", "BC");
                 cipher.init(Cipher.ENCRYPT_MODE, bankPubKey);
                 byte[] ciphertextBytes = cipher.doFinal(sessionKeyAndSignature.getBytes(StandardCharsets.UTF_8));
                 String ciphertext = Base64.getEncoder().encodeToString(ciphertextBytes);
 
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
                 out.writeObject(ciphertext);
                 out.flush();
 
-                // Decrypt
-                /**Cipher cipher2 = Cipher.getInstance("ECIES", "BC");
-                cipher2.init(Cipher.DECRYPT_MODE, privateKey1);
-                byte[] decoded = Base64.getDecoder().decode(ciphertext);
-                byte[] messageBytes = cipher2.doFinal(decoded);
-                String message = new String(messageBytes);
-                System.out.println(message);**/
+                Object inObj = decryptStringWithAES((byte[]) inputStream.readObject(), sessionKey);
+
+                assert inObj != null;
+                if ((inObj.equals("CLIENT_NUMBER_REQUEST"))) {
+                    out.writeObject(encryptStringWithAES(CLIENT_NUMBER, sessionKey));
+                    out.flush();
+                }
+
+                // Retrieve bank's challenge
+                String challenge = decryptStringWithAES((byte[]) inputStream.readObject(), sessionKey);
+                byte[] decodedChallenge = Base64.getDecoder().decode(challenge);
+
+                //TODO grab user's PIN
+
+                assert challenge != null;
+                byte[] signedChallenge = signMessage(decodedChallenge, privateKey);
+                String signedChallengeAndPin =
+                        Base64.getEncoder().encodeToString(signedChallenge)
+                        + "#"
+                        + USER_PIN;
+
+                out.writeObject(encryptStringWithAES(signedChallengeAndPin, sessionKey));
+                out.flush();
+
+                inObj = decryptStringWithAES((byte[]) inputStream.readObject(), sessionKey);
+
+                assert inObj != null;
+                if (inObj.equals("OK")) {
+                    System.out.println("we cool");
+                } else {
+                    System.out.println("we fucked");
+                }
 
             } catch (NoSuchPaddingException e) {
                 throw new RuntimeException(e);
@@ -84,6 +115,8 @@ public class Client {
                 throw new RuntimeException(e);
             } catch (NoSuchProviderException e) {
                 throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
             }
 
             //Exchanging data with the bank
@@ -93,18 +126,15 @@ public class Client {
 
         } catch (IOException | KeyStoreException | CertificateException | NoSuchAlgorithmException ex) {
             throw new RuntimeException(ex);
-        } catch (UnrecoverableKeyException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    private static byte[] signSessionKey(byte[] message, PrivateKey privateKey) {
+    private static byte[] signMessage(byte[] message, PrivateKey privateKey) {
         try {
             Signature signature = Signature.getInstance("SHA384withECDSA");
             signature.initSign(privateKey);
             signature.update(message);
-            byte[] signatureBytes = signature.sign();
-            return signatureBytes;
+            return signature.sign();
         } catch (Exception e) {
             e.printStackTrace();
         }
